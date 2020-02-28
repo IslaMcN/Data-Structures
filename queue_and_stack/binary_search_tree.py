0d84f6310f1d2c3a8db4111c5bbffa295d420820
Isla <fallenacademy@gmail.com>
2020-02-27 17:09:22 -0800

    Working on day2 of bst

--- queue_and_stack/binary_search_tree.py
+++ queue_and_stack/binary_search_tree.py
@@ -12,16 +12,21 @@ class BinarySearchTree:
 
     # Insert the given value into the tree
     def insert(self, value):
-        if value < self.value and self.left is None:
-            self.left = BinarySearchTree(value)
-            return self.left
-        elif value >= self.value and self.right is None:
-            self.right = BinarySearchTree(value)
-            return self.right
-        elif value < self.value:
-            return self.left.insert(value)
-        elif value >= self.value:
-            return self.right.insert(value)
+        node = BinarySearchTree(value)
+        current = self
+        while True:
+            if value < current.value:
+                if current.left == None:
+                    current.left = node
+                    return
+                else:
+                    current = current.left
+            elif value > current.value:
+                if current.right == None:
+                    current.right = node
+                    return
+                else:
+                    current = current.right
             
 
     # Return True if the tree contains the value
@@ -31,17 +36,22 @@ class BinarySearchTree:
         while current is not None:
             if self.value == target:
                 return True
-            # if target < current.value:
-            #     current = current.left
-            #     return contains(current)
-            # elif target > current.value:
-            #     current = current.right
-            #     return contains(current)
+            if target < current.value:
+                if current.left:
+                    return current.left.contains(target)
+                else:
+                    False
+            elif target > current.value:
+                if current.right:
+                    return self.right.contains(target)
+                else:
+                    return False
+
         return False
 
     # Return the maximum value found in the tree
     def get_max(self):
-        if not self.right:
+        if self.right == None:
             return self.value
         else:
             return self.right.get_max()
@@ -49,20 +59,32 @@ class BinarySearchTree:
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
     def for_each(self, cb):
-        cb(self.value)
-        if self.left:
-            cb(self.left.value)
-            return self.left.for_each(cb)
-        if self.right:
-            cb(self.right.value)
+        if self.right != None and self.left != None:
+            cb(self.value)
+            self.left.for_each(cb)
             return self.right.for_each(cb)
+        if self.right == None and self.left != None:
+            cb(self.value)
+            return self.left.for_each(cb)
+        if self.right != None and self.left == None:
+            cb(self.value)
+            return self.right.for_each(cb)
+        if self.right == None and self.left == None:
+            cb(self.value)
+            return
 
     # DAY 2 Project -----------------------
 
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
     def in_order_print(self, node):
-        pass
+        #Does node exist?
+        if node == None:
+            return
+        #Print the data of node
+        print(node.value)
+        BinarySearchTree.in_order_print(self.left)
+        BinarySearchTree.in_order_print(self.right)
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
